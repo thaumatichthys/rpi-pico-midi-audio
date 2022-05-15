8cc8205627f2ca8a6c5025c068597b675dbe607d
thaumatichthys <66721211+thaumatichthys@users.noreply.github.com>
2022-05-15 00:37:36 -0600

    Create midi to text.py

---
+++ midi to text.py
@@ -0,0 +1,70 @@
+#  embarrassingly bad python code
+
+import mido
+import numpy as np
+
+mid = mido.MidiFile('replace with absolute file path of the midi file')
+
+textdestpath = 'replace with absolute file path of output text file'
+
+output_array = []
+time_ac = 0  # time accumulation
+length = 0
+
+def base_wave_function(input):  # change this if you want lol
+    # return np.sin(input)  # sounds boring and is too quiet
+    return ((input / (np.pi / 2)) % 2) - 1
+
+
+for msg in mid:
+    time_ac += msg.time
+    if msg.type == 'note_on' or msg.type == 'note_off':
+        value = int(round(time_ac * 1000))
+        type = 1  # on
+        if msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0):
+            type = 0
+        value &= 0x00FFFFFF
+        value |= (type << 31)
+        value |= (msg.note << 24)  # 0bCNNNNNNNTTTTTTTTTTTTTTTTTTTTTTTT
+        output_array.append(value)
+        length += 1
+
+print("Required flash size: " + str(int(length * 4 / 1000)) + "kb (approx)")
+
+with open(textdestpath, 'w') as f:
+    f.write("#include <pico/stdlib.h>\n\n")
+    f.write("// Written by hand in 29 months, https://github.com/thaumatichthys\n\nconst uint32_t midi_array[] = {")
+    for i in range(length - 1):
+        if not i % 99:
+            f.write("\n   ")
+        f.write(str(output_array[i]) + ", ")
+    f.write(str(output_array[length - 1]) + "\n};")
+    f.write("\n\nconst uint32_t midi_data_length = " + str(length) + ";")
+
+    # Now we generate the sound lookup table
+    sample_rate = 25000
+
+    min_freq = (2 ** (-69 / 12)) * 440
+    duration = 1 / min_freq
+    samples = int(round(duration * sample_rate))
+
+    f.write("\n\nconst uint8_t wave_table[128][" + str(samples) + "] = {\n")
+
+    for i in range(128):
+        freq = (2 ** ((i - 69) / 12)) * 440
+        f.write("   {\n       ")
+        for x in range(samples):
+            if (not x % 230) and (x > 220):
+                f.write("\n       ")
+            sine_output = base_wave_function(2 * np.pi * (freq / min_freq) * (x / samples))
+            f.write(str(int(round((sine_output + 1) * 127))))
+            if not x > (samples - 2):
+                f.write(", ")
+
+        f.write("\n   }")
+        if not i > (128 - 2):
+            f.write(",\n")
+    f.write("\n};\n\n")
+    f.write("const uint32_t wave_table_length = " + str(samples) + ";\n")
+
+    print("Wave table size: " + str(int(round(samples * 128 / 1000))) + "kb (approx)")
